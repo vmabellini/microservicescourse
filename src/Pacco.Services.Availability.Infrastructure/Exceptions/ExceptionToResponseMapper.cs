f6bff85c5336ff4038dbcf8d74c6e41ca8c00190
Vinicius Mabellini <vpmabellini@gmail.com>
2020-11-25 22:03:09 -0300

    Setando middleware pra fazer um mapeamento das exceptions pro retorno da API

---
+++ src/Pacco.Services.Availability.Infrastructure/Exceptions/ExceptionToResponseMapper.cs
@@ -0,0 +1,45 @@
+﻿using Convey;
+using Convey.WebApi.Exceptions;
+using Pacco.Services.Availability.Application.Exceptions;
+using Pacco.Services.Availability.Core.Exceptions;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pacco.Services.Availability.Infrastructure.Exceptions
+{
+    internal sealed class ExceptionToResponseMapper : IExceptionToResponseMapper
+    {
+        private static readonly ConcurrentDictionary<Type, string> Codes = new ConcurrentDictionary<Type, string>();
+
+        public ExceptionResponse Map(Exception exception)
+            => exception switch
+            {
+                DomainException ex => new ExceptionResponse(new { code = GetCode(ex), reason = ex.Message }, System.Net.HttpStatusCode.BadRequest),
+                AppException ex => new ExceptionResponse(new { code = GetCode(ex), reason = ex.Message }, System.Net.HttpStatusCode.BadRequest),
+                _ => new ExceptionResponse(new { code = "error", reason = "There was an error." }, System.Net.HttpStatusCode.InternalServerError)
+            };
+
+        private static string GetCode(Exception exception)
+        {
+            var type = exception.GetType();
+
+            if (Codes.TryGetValue(type, out var code))
+            {
+                return code;
+            }
+
+            var exceptionCode = exception switch
+            {
+                DomainException ex when !string.IsNullOrWhiteSpace(ex.Code) => ex.Code,
+                AppException ex when !string.IsNullOrWhiteSpace(ex.Code) => ex.Code,
+                _ => type.Name.Underscore().Replace("_exception", "")
+            };
+
+            Codes.TryAdd(type, exceptionCode);
+
+            return exceptionCode;
+        }
+    }
+}
--- src/Pacco.Services.Availability.Infrastructure/Mongo/Extensions.cs
+++ src/Pacco.Services.Availability.Infrastructure/Mongo/Extensions.cs
@@ -1,9 +1,11 @@
 ﻿using Convey;
 using Convey.CQRS.Queries;
 using Convey.Persistence.MongoDB;
+using Convey.WebApi;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Pacco.Services.Availability.Core.Repositories;
+using Pacco.Services.Availability.Infrastructure.Exceptions;
 using Pacco.Services.Availability.Infrastructure.Mongo.Documents;
 using Pacco.Services.Availability.Infrastructure.Mongo.Repositories;
 using System;
@@ -19,6 +21,7 @@ namespace Pacco.Services.Availability.Infrastructure.Mongo
             builder.Services.AddTransient<IResourcesRepository, ResourcesMongoRepository>();
 
             builder
+                .AddErrorHandler<ExceptionToResponseMapper>()
                 .AddQueryHandlers()
                 .AddInMemoryQueryDispatcher()
                 .AddMongo()
@@ -31,6 +34,8 @@ namespace Pacco.Services.Availability.Infrastructure.Mongo
         {
             //Executa os IStartupInitializers
             app.UseConvey();
+            //Mapeia os erros ocorridos nos handlers para um formato amigável
+            app.UseErrorHandler();
 
             return app;
         }
